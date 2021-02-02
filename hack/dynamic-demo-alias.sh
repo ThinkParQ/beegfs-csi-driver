12d85410b02a4f05649917b7f0ad69189cc998c7
Eric Weber <eric.weber2@netapp.com>
2021-02-02 14:31:40 -0600

    Move and clean up dynamic-demo.md

--- hack/dynamic-demo-alias.sh
+++ hack/dynamic-demo-alias.sh
@@ -1,32 +1,30 @@
 # Copyright 2021 NetApp, Inc. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0.
 
-# Source this file to execute the complex commands in ../docs/beegfs/dynamic-demo.md without having to list out (or
-# copy and paste) all the parameters.
+# Source this file to simplify the execution of some GRPC calls against a locally running driver.
 
-# TODO(webere): Fix or remove dynamic-demo.md.
-# NOTE: ../docs/beegfs/dynamic-demo.md is OUT OF DATE! These commands work, but they are not reflected correctly in
-# dynamic-demo.md.
+# NOTE: ./dynamic-demo.md explains the operation of some of these commands and documents ways to verify they have run
+# successfully. This file contains more commands than are documented in ./dynamic-demo.md.
 
-# Run the plugin like "sudo bin/beegfs-csi-driver --node-id node1 --cs-data-dir=/tmp/csdatadir -vvvvv".
+# Run the plugin like "sudo bin/beegfs-csi-driver --node-id node1 -v 4 --cs-data-dir /tmp/csdatadir".
 
 # Set SYS_MGMTD_HOST in the environment or default to a BeeGFS file system running on localhost. This change applies
 # at alias time (not at run time).
 SYS_MGMTD_HOST="${SYS_MGMTD_HOST:-localhost}"
 
-mkdir -p /tmp/kubelet/plugins/beegfs/volume1 /tmp/kubelet/pods/pod1 /tmp/kubelet/pods/pod2 /csi-data-dir
+mkdir -p /tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount /tmp/kubelet/pods/pod1/volumes/kubernetes.io~csi/pvc-00000001 /tmp/kubelet/pods/pod2/volume/kubernetes.io~csi/pvc-00000001 /tmp/csdatadir
 
 alias csc='sudo -i csc -e /tmp/csi.sock -l info --with-request-logging --with-response-logging'
-alias createvolume="csc controller create-volume volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --params sysMgmtdHost=${SYS_MGMTD_HOST},volDirBasePath=scratch"
-alias createvolumestripepattern="csc controller create-volume volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --params sysMgmtdHost=${SYS_MGMTD_HOST},volDirBasePath=scratch,stripePattern/storagePoolID=2,stripePattern/chunkSize=1m,stripePattern/numTargets=2"
-alias nodestagevolume="csc node stage --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --vol-context=volDirBasePath=scratch beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodepublishvolume="csc node publish --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 --target-path=/tmp/kubelet/pods/pod1/volumes/volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --vol-context=volDirBasePath=scratch beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodepublishvolumereadonly="csc node publish --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 --target-path=/tmp/kubelet/pods/pod1/volumes/volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --vol-context=volDirBasePath=scratch --read-only beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodepublishvolumepod2="csc node publish --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 --target-path=/tmp/kubelet/pods/pod2/volumes/volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --vol-context=volDirBasePath=scratch beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodepublishvolumepod2readonly="csc node publish --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 --target-path=/tmp/kubelet/pods/pod2/volumes/volume1 --cap MULTI_NODE_MULTI_WRITER,mount, --vol-context=volDirBasePath=scratch --read-only beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodeunpublishvolume="csc node unpublish --target-path=/tmp/kubelet/pods/pod1/volumes/volume1 beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodeunpublishvolumepod2="csc node unpublish --target-path=/tmp/kubelet/pods/pod2/volumes/volume1 beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias nodeunstagevolume="csc node unstage --staging-target-path=/tmp/kubelet/plugins/beegfs/volume1 beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias deletevolume="csc controller delete-volume beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias validatevolumecapabilities="csc controller validate-volume-capabilities --cap MULTI_NODE_MULTI_WRITER,mount, beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
-alias validatevolumecapabilitiesfail="csc controller validate-volume-capabilities --cap MULTI_NODE_MULTI_WRITER,block, beegfs://${SYS_MGMTD_HOST}/scratch/volume1"
+alias createvolume="csc controller create-volume pvc-00000001 --cap MULTI_NODE_MULTI_WRITER,mount, --params sysMgmtdHost=${SYS_MGMTD_HOST},volDirBasePath=kubernetes"
+alias createvolumestripepattern="csc controller create-volume pvc-00000001 --cap MULTI_NODE_MULTI_WRITER,mount, --params sysMgmtdHost=${SYS_MGMTD_HOST},volDirBasePath=kubernetes,stripePattern/storagePoolID=2,stripePattern/chunkSize=1m,stripePattern/numTargets=2"
+alias nodestagevolume="csc node stage --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount --cap MULTI_NODE_MULTI_WRITER,mount, beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodepublishvolume="csc node publish --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount --target-path=/tmp/kubelet/pods/pod1/volumes/kubernetes.io~csi/pvc-00000001/mount --cap MULTI_NODE_MULTI_WRITER,mount, beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodepublishvolumereadonly="csc node publish --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount --target-path=/tmp/kubelet/pods/pod1/volumes/kubernetes.io~csi/pvc-00000001/mount --cap MULTI_NODE_MULTI_WRITER,mount, --read-only beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodepublishvolumepod2="csc node publish --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount --target-path=/tmp/kubelet/pods/pod2/volumes/kubernetes.io~csi/pvc-00000001/mount --cap MULTI_NODE_MULTI_WRITER,mount, beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodepublishvolumepodreadonly2="csc node publish --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount --target-path=/tmp/kubelet/pods/pod2/volumes/kubernetes.io~csi/pvc-00000001/mount --cap MULTI_NODE_MULTI_WRITER,mount, --read-only beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodeunpublishvolume="csc node unpublish --target-path=/tmp/kubelet/pods/pod1/volumes/kubernetes.io~csi/pvc-00000001/mount beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodeunpublishvolumepod2="csc node unpublish --target-path=/tmp/kubelet/pods/pod2/volumes/kubernetes.io~csi/pvc-00000001/mount beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias nodeunstagevolume="csc node unstage --staging-target-path=/tmp/kubelet/plugins/kubernetes.io/csi/pv/pvc-00000001/globalmount beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias deletevolume="csc controller delete-volume beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias validatevolumecapabilities="csc controller validate-volume-capabilities --cap MULTI_NODE_MULTI_WRITER,mount, beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
+alias validatevolumecapabilitiesfail="csc controller validate-volume-capabilities --cap MULTI_NODE_MULTI_WRITER,block, beegfs://${SYS_MGMTD_HOST}/kubernetes/pvc-00000001"
