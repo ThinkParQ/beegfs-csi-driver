5c2e320dc19d14276287893ae4bba3638ee7bd02
Eric Weber <eric.weber2@netapp.com>
2022-04-08 14:34:33 -0500

    Update csi-test dependency to v4.3.0

--- deploy/k8s/deploy_test.go
+++ deploy/k8s/deploy_test.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"testing"
 
+	_ "github.com/onsi/ginkgo" // Don't fail if Ginkgo flags are passed to "go test".
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
